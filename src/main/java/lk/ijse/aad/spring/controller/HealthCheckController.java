d157ac6830b9154ba43b2aa8b3c04d7781cadf8b
menukipeiris <menukimpeiris@gmail.com>
2024-10-17 14:06:38 +0530

    Health check

--- src/main/java/lk/ijse/aad/spring/controller/HealthCheckController.java
+++ src/main/java/lk/ijse/aad/spring/controller/HealthCheckController.java
@@ -1,2 +1,16 @@
-package lk.ijse.aad.spring.controller;public class HealthChechController {
+package lk.ijse.aad.spring.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/health")
+public class HealthCheckController {
+    @GetMapping
+    public String healthCheck() {
+        return "Health controller is working";
+    }
+
 }
